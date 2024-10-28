9b82acf289d0ac578bdaeb62367f5bebad289a4d
AbdulNaved <140192855+AbdulNaved@users.noreply.github.com>
2024-10-28 13:36:19 -0500

    Update route.ts

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,18 +1,18 @@
-import handlers from "@/app/api/auth/[...nextauth]/route"; // Correct relative path
+// import handlers from "@/app/api/auth/[...nextauth]/route"; // Correct relative path
 
-export const { GET, POST } = handlers; // Destructuring GET and POST from handlers
+// export const { GET, POST } = handlers; // Destructuring GET and POST from handlers
 
 
-export async function GET(request: Request) {
-    // Handle GET requests
-    return new Response("GET method from auth");
-  }
+// export async function GET(request: Request) {
+//     // Handle GET requests
+//     return new Response("GET method from auth");
+//   }
   
-  export async function POST(request: Request) {
-    // Handle POST requests
-    const data = await request.json();
-    return new Response(`POST method with data: ${JSON.stringify(data)}`);
-}
+//   export async function POST(request: Request) {
+//     // Handle POST requests
+//     const data = await request.json();
+//     return new Response(`POST method with data: ${JSON.stringify(data)}`);
+// }
 
 // //import handlers from "@/app/api/auth/[...nextauth]/auth"
 // import handlers from "../auth/"; // Assuming `auth.ts` is in the same directory
